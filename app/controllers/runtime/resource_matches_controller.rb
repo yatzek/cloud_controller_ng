961caa01eb275202a1a5c379e3971088938f2a2b
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-03-01 10:50:45 +0000

    Use bits-service for resource matching
    [#113947905]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- app/controllers/runtime/resource_matches_controller.rb
+++ app/controllers/runtime/resource_matches_controller.rb
@@ -5,6 +5,14 @@ module VCAP::CloudController
       return ApiError.new_from_details('NotAuthorized') unless user
       FeatureFlag.raise_unless_enabled!('app_bits_upload') unless SecurityContext.admin?
 
+      if use_bits_client?
+        response = bits_client.matches(body.read)
+        return response.body if response.code.to_i == 200
+
+        error = JSON.parse(response.body)
+        raise Errors::ApiError.new_from_details('BitsServiceInvalidResponse', error['description'])
+      end
+
       begin
         fingerprints_all_clientside_bits = MultiJson.load(body)
       rescue MultiJson::ParseError => e
@@ -17,6 +25,16 @@ module VCAP::CloudController
 
       fingerprints_existing_in_blobstore = ResourcePool.instance.match_resources(fingerprints_all_clientside_bits)
       MultiJson.dump(fingerprints_existing_in_blobstore)
+    end
+
+    private
+
+    def use_bits_client?
+      !!::CloudController::DependencyLocator.instance.use_bits_service
+    end
+
+    def bits_client
+      ::CloudController::DependencyLocator.instance.bits_client
     end
   end
 end
