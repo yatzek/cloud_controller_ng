b74fda32ffeb959e550f99abdab0bbdc85518b94
Amelia Downs <adowns@pivotal.io>
2016-04-21 11:22:55 -0700

    Document experimental v3 droplet audit events
    [#101936042]
    
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'vcap/digester'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '018f17cffc7d8d760fb531223d2018abe5a56140'.freeze
+  API_FOLDER_CHECKSUM = '315306877f8a05c41778bbdb157b009d189bec89'.freeze
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.55.0')
