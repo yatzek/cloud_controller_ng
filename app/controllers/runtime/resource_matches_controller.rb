cb2aa2c4b85631492ef0786e4a5d16216ec68140
Tiago Scolari <tscolari@pivotal.io>
2016-06-27 15:26:29 +0200

    Remove :: from some calls
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- app/controllers/runtime/resource_matches_controller.rb
+++ app/controllers/runtime/resource_matches_controller.rb
@@ -10,18 +10,18 @@ module VCAP::CloudController
           response = bits_service_resource_pool.matches(body.read)
           return response.body
         rescue BitsService::Errors::Error => e
-          raise ::CloudController::Errors::ApiError.new_from_details('BitsServiceError', e.message)
+          raise CloudController::Errors::ApiError.new_from_details('BitsServiceError', e.message)
         end
       end
 
       begin
         fingerprints_all_clientside_bits = MultiJson.load(body)
       rescue MultiJson::ParseError => e
-        raise ::CloudController::Errors::ApiError.new_from_details('MessageParseError', e.message)
+        raise CloudController::Errors::ApiError.new_from_details('MessageParseError', e.message)
       end
 
       unless fingerprints_all_clientside_bits.is_a?(Array)
-        raise ::CloudController::Errors::ApiError.new_from_details('UnprocessableEntity', 'must be an array.')
+        raise CloudController::Errors::ApiError.new_from_details('UnprocessableEntity', 'must be an array.')
       end
 
       fingerprints_existing_in_blobstore = ResourcePool.instance.match_resources(fingerprints_all_clientside_bits)
@@ -31,7 +31,7 @@ module VCAP::CloudController
     private
 
     def bits_service_resource_pool
-      ::CloudController::DependencyLocator.instance.bits_service_resource_pool
+      CloudController::DependencyLocator.instance.bits_service_resource_pool
     end
   end
 end
